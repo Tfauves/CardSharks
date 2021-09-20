5f5b1d8445f619e28a5ef16640d91defef1e40fb
Travis J <tfauvelle@gmail.com>
2021-09-20 18:39:50 -0400

    logic for higher guess.

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -3,6 +3,7 @@ import com.company.cardGame.Card;
 import com.company.cardGame.Deck;
 import com.company.cardGame.StandardDeck;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 
@@ -20,6 +21,15 @@ public class Game {
         System.out.println(card1);
         System.out.println("Is the next card (l)ower or (h)igher?");
         String input = scanner.nextLine();
+        card2 = cardDeck.deal();
+        System.out.println(card2);
+
+        if (input.toLowerCase().equals("h") && card1.getFaceValue() < card2.getFaceValue()) {
+            System.out.println("Your correct");
+
+        } else {
+            System.out.println("loser");
+        }
     }
 
 }
