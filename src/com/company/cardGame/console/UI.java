9115c952ae1c9fee9774b86ce40b9b6efe348a5f
Travis J <tfauvelle@gmail.com>
2021-10-27 09:00:07 -0400

    get int int ui.

--- src/com/company/cardGame/console/UI.java
+++ src/com/company/cardGame/console/UI.java
@@ -5,4 +5,19 @@ import java.util.Scanner;
 public class UI {
     public static final Scanner scanner = new Scanner(System.in);
 
+    public int getInt(String prompt, int min, int max, String errMsg) {
+        int option = min -1;
+        do {
+            System.out.println(prompt);
+            String input = scanner.nextLine();
+            try {
+                option = Integer.parseInt(input);
+            }catch (NumberFormatException err) {
+                System.out.println(errMsg);
+            }
+        } while (option < min && option > max);
+
+        return option;
+    }
+
 }
