8d705fed6bc973040117a13b88abd5ad8db2fbbb
Travis J <tfauvelle@gmail.com>
2022-11-25 15:31:49 -0500

    match, run and player deck

--- src/com/company/cardGame/actor/Player.java
+++ src/com/company/cardGame/actor/Player.java
@@ -1,5 +1,67 @@
 package com.company.cardGame.actor;
 
-public class Player {
+import com.company.cardGame.cardSharks.Run;
+import com.company.cardGame.deck.PlayerDeck;
 
+public class Player {
+    private String name;
+    private PlayerDeck playerDeck;
+    private Run run;
+    private boolean isTurn;
+    private int matchWins;
+    private int gameWins;
+
+    public Player() {};
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PlayerDeck getPlayerDeck() {
+        return playerDeck;
+    }
+
+    public void setPlayerDeck(PlayerDeck playerDeck) {
+        this.playerDeck = playerDeck;
+    }
+
+    public Run getRun() {
+        return run;
+    }
+
+    public void setRun(Run run) {
+        this.run = run;
+    }
+
+    public boolean isTurn() {
+        return isTurn;
+    }
+
+    public void setTurn(boolean turn) {
+        isTurn = turn;
+    }
+
+    public int getMatchWins() {
+        return matchWins;
+    }
+
+    public void setMatchWins(int matchWins) {
+        this.matchWins = matchWins;
+    }
+
+    public int getGameWins() {
+        return gameWins;
+    }
+
+    public void setGameWins(int gameWins) {
+        this.gameWins = gameWins;
+    }
 }
--- src/com/company/cardGame/deck/Card.java
+++ src/com/company/cardGame/deck/Card.java
@@ -1,6 +1,5 @@
 package com.company.cardGame.deck;
 
-
 public class Card {
     private final String suit;
     private final int faceValue;
@@ -9,7 +8,6 @@ public class Card {
     public Card(String suit, int faceValue) {
         this.suit = suit;
         this.faceValue = faceValue;
-
     }
 
     public String toString() {
@@ -18,14 +16,27 @@ public class Card {
 
     public String display() {
         String outPut = "";
-        switch (faceValue) {
-            case 14 -> outPut = "AC";
-            case 11 -> outPut = "JK";
-            case 12 -> outPut = "QU";
-            case 13 -> outPut = "KI";
-            default -> outPut = faceValue == 10 ? Integer.toString(faceValue) : " " + faceValue;
-        };
-        return outPut + " " + suit;
+        if (isFaceUp) {
+            switch (faceValue) {
+                case 14 -> outPut = "AC";
+                case 11 -> outPut = "JK";
+                case 12 -> outPut = "QU";
+                case 13 -> outPut = "KI";
+                default -> {
+                    outPut = faceValue == 10 ? Integer.toString(faceValue) : " " + faceValue;
+                    outPut = outPut + " " + suit;
+                }
+            };
+        } else {
+            outPut = "back of card";
+        }
+
+        return outPut;
+    }
+
+    // TODO: 11/25/2022 test method 
+    public void turnCard() {
+        setFaceUp(!isFaceUp);
     }
 
     public int getFaceValue() {
